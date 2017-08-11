29adb95eab1a07303706ed453e60a5aaafe8bd6d
liones3000 <liones3000@gmail.com>
2017-08-11 14:28:30 +0300

    Done

--- js/functions.js
+++ js/functions.js
@@ -13,35 +13,44 @@ function countStatus(){
 
 function addTask(item, id){ //add task to page
 	var $remove = $('<button>').addClass('btn btn-danger btn-xs pull-right btn-remove mar5').append('<span class="glyphicon glyphicon-remove">');
-	var $edit = $('<button>').addClass('btn btn-warning btn-xs pull-right btn-edit').append('<span class="glyphicon glyphicon-pencil">');
-// debugger;
+	var $edit = $('<button>').addClass('btn btn-warning btn-xs pull-right btn-edit mar5').append('<span class="glyphicon glyphicon-pencil">');
+	var $view = $('<button>').addClass('btn btn-info btn-xs pull-right btn-view').append('<span class="glyphicon glyphicon-eye-open">')
+	.on('click', function(){
+		var $id = $(this).parents('[data-id]').attr('data-id');
+		$('#'+$id).collapse('toggle');
+	});
+		// Additional method
 	var $a = $('<a>')
-	.attr({'data-toogle':'collapse', 'data-parent': '#accordion', 'href':'#'+id, 'aria-expanded':'true', 'aria-controls':id})
+	.attr({'data-toogle':'collapse', 'href':'#'+id})
 	.text(item.title)
 	.on('click', function(){
-		$('#accordion').collapse();
-	})
-
+		var $id = $(this).attr('href');
+		$($id).collapse('toggle');
+	});
+		// Additional method end
+		
 	var $panelTitle = $('<h4>')
 	.addClass('panel-title')
 	.append($a)
 	.append($remove)
-	.append($edit);
+	.append($edit)
+	.append($view);
 
 	var $panelHeading = $('<div>')
 	.addClass('panel-heading')
-	.attr('role','tab')
-	.attr('id', 'one')
 	.append($panelTitle);
+
+	var $date = $('<p>').addClass('small').html('<i class="glyphicon glyphicon-calendar"><strong>'+item.date+'</strong></i>');
+	var $description = $('<div>').text(item.description);
 
 	var $panelBody = $('<div>')
 	.addClass('panel-body')
-	.text(item.description);
+	.append($date)
+	.append($description);
 
 	var $collapse = $('<div>')
-	.attr({'aria-labelledby':'one', 'role':'tabepanel'})
 	.attr('id', ''+id)
-	.addClass('panel-collapse collapse in')
+	.addClass('panel-collapse collapse')
 	.append($panelBody);
 
 	var $div = $('<div>')
@@ -49,6 +58,6 @@ function addTask(item, id){ //add task to page
 	.appendTo('[data-status="' + item.status + '"] .panel-group')
 	.attr('data-id', id)
 	.append($panelHeading)
-	.append($collapse)
+	.append($collapse);
 
 } //end addTask
